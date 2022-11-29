3bb8c8a22e50f25dd6b47a97bb9ad4ee0d2c0853
Shachar96 <shachar.ketz@gmail.com>
2022-11-29 12:48:12 +0200

    string test

--- ttt.c
+++ ttt.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 int main(){
-    printf("hello");
+    char s1[] = "MyNameIs Shachar";
+    char s2[50] = "ILoveIsrael ";
+    char s3[] = {'I','L','o','v','e','J','\0'};
+    printf("%s\n%s%s",s1,s2,s3);
     return 0;
 }
